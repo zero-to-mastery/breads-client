2911cf66b0e347b3047116556ffcdac0230097a4
aTmb405 <aTmb405@gmail.com>
2020-04-17 17:10:07 -0500

    Fix default axios url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080/api';
+// axios.defaults.baseURL = 'http://localhost:8080/api';
+axios.defaults.baseURL = 'https://breads-server.herokuapp.com/api';
 
 export function setTokenHeader(token) {
     if (token) {
