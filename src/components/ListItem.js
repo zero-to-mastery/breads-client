fdc92f2714a717777b4c7b43859ccf7765b3f65d
aTmb405 <aTmb405@gmail.com>
2020-05-28 18:43:46 -0500

    Fix date display error

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ListItem = props => {
     console.log(props.favorite);
+    console.log(props.date);
     return (
         <li style={props.style} className='list-group-item border-secondary'>
             <h5 className='row'><a href={`${props.url}`} target='_blank'  rel='noopener noreferrer' className='text-primary'><strong>{props.title}</strong></a></h5>
@@ -37,7 +38,7 @@ const ListItem = props => {
                     </div>
                 }
                 <Moment className='text-muted mt-3 ml-2' fromNow ago>
-                    <small>{props.date}</small>
+                    {props.date}
                 </Moment> 
                 {props.loading.isLoading && props.loading.id === props.id
                     ? <p onClick={props.viewSummary} className='btn text-muted m-2 ml-auto'>
