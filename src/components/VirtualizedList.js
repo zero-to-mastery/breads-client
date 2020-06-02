594d421470c9c309ad54a721b1d26cede7badacd
aTmb405 <aTmb405@gmail.com>
2020-06-01 23:52:54 -0500

    Refactor lists to use common VirtualizedList component

---
+++ src/components/VirtualizedList.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import ListItem from './ListItem';
+import { List, AutoSizer, CellMeasurer, CellMeasurerCache, WindowScroller } from 'react-virtualized';
+
+const VirtualizedList = props => {
+        const {
+            readings,
+            summary,
+            fetchSummary,
+            removeSummary,
+            postNewSubscription,
+            loading,
+            removeUserReading,
+            currentUser,
+            markFavorite,
+            unfavorite
+        } = props;
+
+        const r = readings.data;
+
+        const cache = new CellMeasurerCache({
+            fixedWidth: true,
+            defaultHeight: 187
+        });
+
+        const renderRow = ({ index, key, parent, style }) => {
+            return (
+                <CellMeasurer
+                    rowIndex={index}
+                    columnIndex={0}
+                    key={key}
+                    cache={cache}
+                    parent={parent}
+                    enableMargins
+                >
+                    <ListItem 
+                        key={key}
+                        id={r[index].id}
+                        title={r[index].title}
+                        domain={r[index].domain}
+                        url={r[index].url}
+                        word_count={r[index].word_count}
+                        user_id={r[index].user_id}
+                        date={r[index].created_at}
+                        username={r[index].username}
+                        image={r[index].image}
+                        summary={summary.summary}
+                        viewSummary={fetchSummary.bind(this, r[index].id, r[index].article_url)}
+                        removeSummary={removeSummary}
+                        loading={loading}
+                        style={style}
+                        favorite={r[index].favorite === currentUser}
+                        newSubscription={postNewSubscription ? postNewSubscription.bind(this, r[index].user_id) : undefined}
+                        removeReading={removeUserReading ? removeUserReading.bind(this, r[index].user_id, r[index].id) : undefined}
+                        markFavorite={markFavorite ? markFavorite.bind(this, r[index].id) : undefined}
+                        unfavorite={unfavorite ? unfavorite.bind(this, r[index].id) : undefined}
+                        isCorrectUser={currentUser ? currentUser === r[index].user_id : undefined}
+                    />
+                </CellMeasurer>
+            );
+        };
+    return (
+        <div className='col-lg-6 col-md-8 col-sm-10 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-0'>
+            <WindowScroller>
+                {({ height, isScrolling, onChildScroll, scrollTop }) => (
+                    <div className='list-group' id='list_data'>
+                        <AutoSizer disableHeight>
+                            {({ width }) => (
+                                <List 
+                                    width={width}
+                                    height={height}
+                                    deferredMeasurementCache={cache}
+                                    rowHeight={cache.rowHeight}
+                                    rowRenderer={renderRow}
+                                    rowCount={r.length}
+                                    autoHeight
+                                    scrollTop={scrollTop}
+                                    isScrolling={isScrolling}
+                                    onChildScroll={onChildScroll}
+                                    style={{ outline: 'none' }}
+                                />
+                            )}
+                        </AutoSizer> 
+                    </div>
+                )}
+            </WindowScroller>
+        </div>
+    )
+}
+
+export default VirtualizedList;
--- src/containers/UserReadingsList.js
+++ src/containers/UserReadingsList.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUserReadings, removeUserReading, markFavorite, unfavorite } from '../store/actions/userReadings';
 import { fetchSummary, removeSummary } from '../store/actions/summary';
-import ListItem from '../components/ListItem';
-import { List, AutoSizer, CellMeasurer, CellMeasurerCache, WindowScroller } from 'react-virtualized';
+import VirtualizedList from '../components/VirtualizedList';
 
 class UserReadingsList extends Component {
     componentDidMount() {
@@ -18,76 +17,19 @@ class UserReadingsList extends Component {
 
     render() {
         const { readings, removeUserReading, summary, fetchSummary, removeSummary, currentUser, markFavorite, unfavorite, loading } = this.props;
-
-        const cache = new CellMeasurerCache({
-            fixedWidth: true,
-            defaultHeight: 187
-        });
-
-        let x = [];
-        if (readings) x = readings.data;
         
-        const renderRow = ({ index, key, parent, style }) => {
-            return (
-                <CellMeasurer
-                    rowIndex={index}
-                    columnIndex={0}
-                    key={key}
-                    cache={cache}
-                    parent={parent}
-                    enableMargins
-                >
-                    <ListItem 
-                        key={key}
-                        id={x[index].id}
-                        title={x[index].title}
-                        domain={x[index].domain}
-                        url={x[index].url}
-                        word_count={x[index].word_count}
-                        user_id={x[index].user_id}
-                        date={x[index].created_at}
-                        username={x[index].username}
-                        image={x[index].image}
-                        summary={summary.summary}
-                        viewSummary={fetchSummary.bind(this, x[index].id, x[index].article_url)}
-                        removeSummary={removeSummary}
-                        removeReading={removeUserReading.bind(this, x[index].user_id, x[index].id)}
-                        favorite={x[index].favorite === currentUser}
-                        markFavorite={markFavorite.bind(this, x[index].id)}
-                        unfavorite={unfavorite.bind(this, x[index].id)}
-                        isCorrectUser={currentUser === x[index].user_id}
-                        loading={loading}
-                        style={style}
-                    />
-                </CellMeasurer>
-            );
-        };
         return (
-            <div className='col-lg-6 col-md-8 col-sm-10 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-0'>
-                <WindowScroller>
-                    {({ height, isScrolling, onChildScroll, scrollTop }) => (
-                        <div className='list-group' id='list_data'>
-                            <AutoSizer disableHeight>
-                                {({ width }) => (
-                                    <List 
-                                        width={width}
-                                        height={height}
-                                        deferredMeasurementCache={cache}
-                                        rowHeight={cache.rowHeight}
-                                        rowRenderer={renderRow}
-                                        rowCount={x.length}
-                                        autoHeight
-                                        scrollTop={scrollTop}
-                                        isScrolling={isScrolling}
-                                        onChildScroll={onChildScroll}
-                                        style={{ outline: 'none' }}
-                                    />
-                                )}
-                            </AutoSizer> 
-                        </div>
-                    )}
-                </WindowScroller>
-            </div>
+            <VirtualizedList 
+                readings={readings}
+                summary={summary}
+                fetchSummary={fetchSummary}
+                removeSummary={removeSummary}
+                removeUserReading={removeUserReading}
+                currentUser={currentUser}
+                markFavorite={markFavorite}
+                unfavorite={unfavorite}
+                loading={loading}
+            />
         )
     }
 }
