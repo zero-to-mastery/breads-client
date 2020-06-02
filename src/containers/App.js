6dbdc80685b5028c1a35c3de99c12eafe0ba639f
aTmb405 <aTmb405@gmail.com>
2020-06-01 23:02:11 -0500

    Update favicon logo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Routes from './Routes';
-import { Provider } from 'react-redux'; //makes react and redux connect together
+import { Provider } from 'react-redux';
 import { configureStore } from '../store';
 import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
 import jwtDecode from 'jwt-decode';
 
-const store = configureStore(); //the store controls all of the state in app
+const store = configureStore();
 
-//persistent jwtToken storage
 if (localStorage.jwtToken) { 
     setAuthorizationToken(localStorage.jwtToken);
-    //prevent someone from manually tampering with the key of jwtToken in localStorage
     try {
         store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
     } catch (err) {
