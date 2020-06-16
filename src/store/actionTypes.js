b7030443d43697f344a51ad19446dc0f68d509f3
aTmb405 <aTmb405@gmail.com>
2020-06-16 15:14:42 -0500

    Organize action types

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,22 +1,45 @@
+//errors
 export const ADD_ERROR = 'ADD_ERROR';
 export const REMOVE_ERROR = 'REMOVE_ERROR';
+
+//auth
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+
+//readings
 export const LOAD_READINGS = 'LOAD_READINGS';
 export const REMOVE_READING = 'REMOVE_READING';
+
+//summary
 export const LOAD_SUMMARY = 'LOAD_SUMMARY';
 export const REMOVE_SUMMARY = 'REMOVE_SUMMARY';
+
+//user
 export const LOAD_USER = 'LOAD_USER';
+
+//subscriptionReadings
 export const LOAD_SUBSCRIPTION_READINGS = 'LOAD_SUBSCRIPTION_READINGS';
+
+//subscriptions
 export const LOAD_SUBSCRIPTIONS = 'LOAD_SUBSCRIPTIONS';
 export const REMOVE_SUBSCRIPTIONS = 'REMOVE_SUBSCRIPTIONS';
+
+//notifications
 export const LOAD_NOTIFICATIONS = 'LOAD_NOTIFICATIONS';
 export const REMOVE_NOTIFICATIONS = 'REMOVE_NOTIFICATIONS';
+
+//loader
 export const ADD_LOADER = 'ADD_LOADER';
 export const REMOVE_LOADER = 'REMOVE_LOADER';
-export const LOAD_USER_READINGS = 'LOAD_USER_READINGS';
-export const REMOVE_USER_READING = 'REMOVE_USER_READING';
+
+//search
 export const LOAD_SEARCH_RESULTS = 'LOAD_SEARCH_RESULTS';
 export const REMOVE_SEARCH_RESULTS = 'REMOVE_SEARCH_RESULTS';
+
+//favReadings
 export const LOAD_FAVORITES = 'LOAD_FAVORITES';
+
+// user readings
 export const ADD_FAVORITE = 'ADD_FAVORITE';
 export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
+export const LOAD_USER_READINGS = 'LOAD_USER_READINGS';
+export const REMOVE_USER_READING = 'REMOVE_USER_READING';
