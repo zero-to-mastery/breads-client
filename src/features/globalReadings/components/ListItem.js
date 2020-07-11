e4f81974f52892e170729275aaa2dbadd23984ba
aTmb405 <aTmb405@gmail.com>
2020-07-11 17:48:26 -0500

    Revert back to function component

--- src/features/globalReadings/components/ListItem.js
+++ src/features/globalReadings/components/ListItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
@@ -8,82 +8,42 @@ import Favorites from './Favorites';
 import Delete from './Delete';
 import { getReadingById } from '../selectors';
 
-class ListItem extends Component { // change back to function component
-    // decouple/consolidate reading lists - CHECK
-        // global and subscription - check
-        // fav and user - check
-        // user and global - check
-    // make each 'handle' function its own component - CHECK
-    // new reading adds to db, but not to state - CHECK not really possible because python has to parse url
-    // are readingLists in state derived data? no, just arrays. could turn into selectors - CHECK
-    // markFav and unFav don't work because there's no favorite reducer now - CHECK
-    // deleteReading deletes from db, but not from state - CHECK
-    // delete Reading only deletes from one reading list, other's keep the deleted reading - CHECK
-    // if I unfollow someone, their readings still show up in my subscription feed <----- see below - CHECK
-    // no websites - derived data - CHECK
-    // no top website - CHECK
-    // make sure listcard, aside work like listitem - pull own data from ids
-        // aside - CHECK
-        // listcard
-    // if I follow someone, their readings don't get added in feed
-        // add reading action - not sure if possible
-        // add subscription action - CHECK
-    // if subsReads are empty, it emptys readings state -- this affects logging out too - CHECK
-        // because entities.readings is empty - CHECK
-        // give fetching to the aside instead? - CHECK
-        // aside data is coupled to reading data! - CHECK
-        // happens with subscriptions too - cannot read prop map of undefined - CHECK
-        // if user has no readings, profile doesn't display --> user state grabbed from readings - CHECK
-            // if user doesn't have subscriptions, state doesn't initiate array to add new sub to - CHECK
-            // no readings are loaded on 'subscriptions' url - CHECK
-            // get info from user or currentuser - CHECK
-            // global readings doesn't reload whenever new reading is added while in user reads - CHECK
-    // move actiontypes into one file - CHECK
-    // better imports in global readings (loader and errors) - CHECK
+const ListItem = props => {
+    const { id, style, users, reading, summary } = props;
+    return (
+        <li style={style} className='list-group-item border-secondary'>
+            <h5 className='row'><a href={`${reading.url}`} target='_blank'  rel='noopener noreferrer' className='text-primary'><strong>{reading.title}</strong></a></h5>
+            <div className='row reading-area'>
+                <p className='lead'>{reading.domain}</p>
+                <p className='text-muted ml-auto'>~{Number(reading.word_count).toLocaleString()} words</p>             
+            </div>
+            <div className='row'>
+                <UserImage
+                    image={users[reading.reader].image}
+                    username={users[reading.reader].username}
+                    class='timeline-image'
+                    height='48'
+                    width='48'
+                />
+                {!props.isCorrectUser && 
+                    <Link to={`/${users[reading.reader].id}`} className='btn text-primary m-2'>
+                        <small>{users[reading.reader].username}</small>
+                    </Link>    
+                }
+                <Moment className='text-muted mt-3 ml-2' fromNow ago>
+                    {reading.created_at}
+                </Moment> 
 
+                <Summary id={id}/>
+                <Favorites id={id} reader={reading.reader} favorite={reading.favorite}/>
+                <Delete id={id} reader={reading.reader}/>
 
-    // flash loads currentUser first on reload - not too important
-        
-    
-    // use RESELECT
-    
-    render() {
-        const { id, style, users, reading, summary } = this.props;
-        return (
-            <li style={style} className='list-group-item border-secondary'>
-                <h5 className='row'><a href={`${reading.url}`} target='_blank'  rel='noopener noreferrer' className='text-primary'><strong>{reading.title}</strong></a></h5>
-                <div className='row reading-area'>
-                    <p className='lead'>{reading.domain}</p>
-                    <p className='text-muted ml-auto'>~{Number(reading.word_count).toLocaleString()} words</p>             
-                </div>
-                <div className='row'>
-                    <UserImage
-                        image={users[reading.reader].image}
-                        username={users[reading.reader].username}
-                        class='timeline-image'
-                        height='48'
-                        width='48'
-                    />
-                    {!this.props.isCorrectUser && 
-                        <Link to={`/${users[reading.reader].id}`} className='btn text-primary m-2'>
-                            <small>{users[reading.reader].username}</small>
-                        </Link>    
-                    }
-                    <Moment className='text-muted mt-3 ml-2' fromNow ago>
-                        {reading.created_at}
-                    </Moment> 
-
-                    <Summary id={id}/>
-                    <Favorites id={id} reader={reading.reader} favorite={reading.favorite}/>
-                    <Delete id={id} reader={reading.reader}/>
-
-                    {summary.id == reading.id &&
-                        <p className='summary-data'>{summary.data}</p>
-                    }
-                </div>
-            </li>
-        )
-    }
+                {summary.id == reading.id &&
+                    <p className='summary-data'>{summary.data}</p>
+                }
+            </div>
+        </li>
+    )
 }
 
 function mapStateToProps(state, ownProps) {
