94024cb6ad8d7f49ec35ec7305a1a338292a6bd8
aTmb405 <aTmb405@gmail.com>
2020-07-17 13:55:17 -0500

    Add reading image and description to listitem

--- src/features/globalReadings/components/ListItem.js
+++ src/features/globalReadings/components/ListItem.js
@@ -12,36 +12,45 @@ const ListItem = props => {
     const { id, style, users, reading, summary } = props;
     return (
         <li style={style} className='list-group-item border-secondary'>
-            <h5 className='row'><a href={`${reading.url}`} target='_blank'  rel='noopener noreferrer' className='text-primary'><strong>{reading.title}</strong></a></h5>
-            <div className='row reading-area'>
-                <p className='lead'>{reading.domain}</p>
-                <p className='text-muted ml-auto'>{Math.round(reading.word_count / 300)} min read</p>             
-            </div>
-            <div className='row'>
-                <UserImage
-                    image={users[reading.reader].image}
-                    username={users[reading.reader].username}
-                    class='timeline-image'
-                    height='48'
-                    width='48'
-                />
-                {!props.isCorrectUser && 
-                    <Link to={`/${users[reading.reader].id}`} className='btn text-primary m-2'>
-                        <small>{users[reading.reader].username}</small>
-                    </Link>    
-                }
-                <Moment className='text-muted mt-3 ml-2' fromNow ago>
-                    {reading.created_at}
-                </Moment> 
+            <img
+                src={reading.reading_image}
+                className='card-img'
+            />
+            <div className='special card-img-overlay'>
+                <h5 className='card-title row m-2'><a href={`${reading.url}`} target='_blank'  rel='noopener noreferrer' className='text-primary'><strong>{reading.title}</strong></a></h5>
+                <p className='card-text small m-2'>{reading.description}</p>
+                <div className='card-text row m-2'>
+                {/* <div className='card-text row reading-area'> */}
+                    <p className='lead'>{reading.domain}</p>
+                    <p className='text-muted ml-auto'>{Math.round(reading.word_count / 300)} min read</p>             
+                </div>
+                <div className='card-text row m-2'>
+                    <UserImage
+                        image={users[reading.reader].image}
+                        username={users[reading.reader].username}
+                        class='timeline-image'
+                        height='48'
+                        width='48'
+                    />
+                    {!props.isCorrectUser && 
+                        <Link to={`/${users[reading.reader].id}`} className='btn text-primary m-2'>
+                            <small>{users[reading.reader].username}</small>
+                        </Link>    
+                    }
+                    <Moment className='text-muted mt-3 ml-2' fromNow ago>
+                        {reading.created_at}
+                    </Moment> 
 
-                <Summary id={id}/>
-                <Favorites id={id} reader={reading.reader} favorite={reading.favorite}/>
-                <Delete id={id} reader={reading.reader}/>
+                    <Summary id={id}/>
+                    <Favorites id={id} reader={reading.reader} favorite={reading.favorite}/>
+                    <Delete id={id} reader={reading.reader}/>
 
-                {summary.id == reading.id &&
-                    <p className='summary-data'>{summary.data}</p>
-                }
+                    {summary.id == reading.id &&
+                        <p className='summary-data'>{summary.data}</p>
+                    }
+                </div>
             </div>
+            
         </li>
     )
 }
