12c5d919348e7c154025908ee617b0231ea8429f
aTmb405 <aTmb405@gmail.com>
2020-08-15 15:29:23 -0500

    Add selectors to index

--- src/features/tags/index.js
+++ src/features/tags/index.js
@@ -1,7 +1,7 @@
 import * as actions from './actions';
 import * as constants from './constants';
 import reducer from './reducer';
-// import * as selectors from './selectors'
+import * as selectors from './selectors'
 
 export { default as TagsAside } from './components/TagsAside';
-export default { actions, constants, reducer };
+export default { actions, constants, reducer, selectors };
