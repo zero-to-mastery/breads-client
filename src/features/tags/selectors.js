4f69bb583dbd3264081b1b5a6e9e1f50c8aacffd
aTmb405 <aTmb405@gmail.com>
2020-08-16 13:30:49 -0500

    Only return 10 tags for most recent and top tags

--- src/features/tags/selectors.js
+++ src/features/tags/selectors.js
@@ -6,7 +6,8 @@ export const getMostRecentTags = (state) => {
     if (state && state[NAME]) {
         let tags = Object.values(state[NAME])
                         .sort((a, b) => a.date > b.date)
-                        .map(tag => state[NAME][tag.id].id).reverse();
+                        .map(tag => state[NAME][tag.id].id).reverse()
+                        .slice(0, 10);
         return tags;
     }
 }
@@ -15,7 +16,8 @@ export const getTopTags = (state) => {
     if (state && state[NAME]) {
         let tags = Object.values(state[NAME])
                         .sort((a, b) => b.count - a.count)
-                        .map(tag => state[NAME][tag.id].id);
+                        .map(tag => state[NAME][tag.id].id)
+                        .slice(0, 10);
         return tags;
     }
 }
