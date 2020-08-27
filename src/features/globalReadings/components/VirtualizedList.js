dfdef54a2eb56386ca7275fedd78cb982525e625
aTmb405 <aTmb405@gmail.com>
2020-08-27 16:24:20 -0500

    Add modal

--- src/features/globalReadings/components/VirtualizedList.js
+++ src/features/globalReadings/components/VirtualizedList.js
@@ -63,6 +63,26 @@ const VirtualizedList = props => {
                     )}
                 </WindowScroller>
             }
+            {/* Modal placed here for visibility. It doesn't display properly when placed inside Listitem */}
+            <div className='modal fade' id='exampleModal' tabIndex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                <div className='modal-dialog' role='document'>
+                    <div className='modal-content'>
+                        <div className='modal-header'>
+                            <h5 className='modal-title' id='exampleModalLabel'>Modal title</h5>
+                            <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+                                <span aria-hidden='true'>&times;</span>
+                            </button>
+                        </div>
+                        <div className='modal-body'>
+                        ...
+                        </div>
+                        <div className='modal-footer'>
+                            <button type='button' className='btn btn-secondary' data-dismiss='modal'>Close</button>
+                            <button type='button' className='btn btn-primary'>Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
