4f0414b0ea7933f1489623257e8cec83a742d426
aTmb405 <aTmb405@gmail.com>
2020-09-26 20:32:37 -0500

    Increase reading image resolution after its transformation

--- src/features/globalReadings/components/ListItem.js
+++ src/features/globalReadings/components/ListItem.js
@@ -13,31 +13,39 @@ import BreadsImage from '../../../images/breads-wesual-click.jpg'
 
 const ListItem = props => {
     const { id, style, list, users, reading, summary, currentUser, outdated, tags } = props;
-    
     const minutes = Math.round(reading.word_count / 300);
 
-    // use images.webserv.nl to serve http images as https
-    let backgroundImg = '';
-    if (reading && reading.reading_image) {
-        backgroundImg = `https://images.weserv.nl/?url=${reading.reading_image}&w=167&output=webp`;
-        // backgroundImg = reading.reading_image.includes('http://') ? `https://images.weserv.nl/?url=${reading.reading_image}&output=png` : reading.reading_image
+    function addImageTransformation(image) {
+        let imageURL = new URL(image);
+        let pathnameArray = imageURL.pathname.split('/');
+        let originalPathnameArray = pathnameArray.slice();
+        for (let i = 0; i < originalPathnameArray.length; i++) {
+            if (pathnameArray[i] === 'upload') {
+                pathnameArray.splice(i + 1, 0, 'w_96,h_96,c_fill,g_face');
+                imageURL.pathname = pathnameArray.join('/');
+                return imageURL.href;
+            }
+        }
     }
-    
+
+    function serveImageThroughCDN() {
+        if (reading && reading.reading_image) {
+            return `https://images.weserv.nl/?url=${reading.reading_image}&w=167&output=webp`;
+        }
+    }
+
+    // let newUserImage = users[reading.reader].image;
+    let newUserImage = addImageTransformation(users[reading.reader].image);
+    let newReadingImage = serveImageThroughCDN() || reading.reading_image;    
+    console.log(newUserImage);
     return (
-        <li 
-            style={{
-                ...style,
-                // backgroundImage: `url(${backgroundImg || BreadsImage})`,
-                // backgroundSize: 'cover'
-            }}
+        <li style={{ ...style }}
             className='card border-secondary'
             id='list-item'
-            // className='list-group-item p-0 overflow-hidden'
         >
             <div className='row'>
-                {/* <div className='h-100 special d-flex flex-column justify-content-around m-1 p-2'> */}
                 <div className='col-md-4'>
-                    <img loading='lazy' src={backgroundImg || BreadsImage} className='card-img m-3' alt='Article'></img>
+                    <img loading='lazy' src={newReadingImage || BreadsImage} className='card-img m-3' alt='Article'></img>
                 </div>
                 <div className='col-md-8'>
                     <div className='m-3'>
@@ -50,49 +58,47 @@ const ListItem = props => {
                     </div>
                 </div>
             </div>
-            {/* <div className='row'> */}
-                <div className='card-text row flex-nowrap ml-3 mr-3'>
-                    <UserImage
-                        image={users[reading.reader].image}
-                        username={users[reading.reader].username}
-                        className='timeline-image'
-                        height='48'
-                        width='48'
-                    />
+            <div className='card-text row flex-nowrap ml-3 mr-3'>
+                <UserImage
+                    image={newUserImage}
+                    username={users[reading.reader].username}
+                    className='timeline-image'
+                    height='48'
+                    width='48'
+                />
 
-                    {!props.isCorrectUser && 
-                        <Link to={`/${users[reading.reader].id}`} className='btn text-primary m-2'>
-                            <small>{users[reading.reader].username}</small>
-                        </Link>    
-                    }
+                {!props.isCorrectUser && 
+                    <Link to={`/${users[reading.reader].id}`} className='btn text-primary m-2'>
+                        <small>{users[reading.reader].username}</small>
+                    </Link>
+                }
 
-                    <Moment className='text-muted text-nowrap mt-3 ml-2 mr-auto' fromNow ago>
-                        {reading.created_at}
-                    </Moment> 
+                <Moment className='text-muted text-nowrap mt-3 ml-2 mr-auto' fromNow ago>
+                    {reading.created_at}
+                </Moment> 
 
-                    {/* {minutes > 0 && 
-                        <Summary id={id}/>
-                    } */}
-                    {tags && 
-                        <Tags reading={reading} tags={tags} list={list}/>
-                    }
-                    {(list !== 'global' && list !== 'subscriptions') &&
-                        <>
-                            <Favorites id={id} reader={reading.reader} favorite={reading.favorite}/>
-                            <Delete id={id} reader={reading.reader}/>
-                        </>
-                    }
+                {/* {minutes > 0 && 
+                    <Summary id={id}/>
+                } */}
+                {tags && 
+                    <Tags reading={reading} tags={tags} list={list}/>
+                }
+                {(list !== 'global' && list !== 'subscriptions') &&
+                    <>
+                        <Favorites id={id} reader={reading.reader} favorite={reading.favorite}/>
+                        <Delete id={id} reader={reading.reader}/>
+                    </>
+                }
 
-                    {summary.id === reading.id &&
-                        <p className='summary-data'>{summary.data}</p>
-                    }
+                {summary.id === reading.id &&
+                    <p className='summary-data'>{summary.data}</p>
+                }
 
-                    {(currentUser.user.id === reading.reader || currentUser.user.id === 1)&& 
-                    outdated === 'true' &&
-                        <Update user_id={reading.reader} reading_id={id} url={reading.url}/>
-                    }
-                </div>
-            {/* </div> */}
+                {(currentUser.user.id === reading.reader || currentUser.user.id === 1)&& 
+                outdated === 'true' &&
+                    <Update user_id={reading.reader} reading_id={id} url={reading.url}/>
+                }
+            </div>
         </li>
     )
 }
