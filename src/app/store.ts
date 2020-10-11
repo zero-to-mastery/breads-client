520415c5d5ec6bd9306b068adad38c4a4d793482
aTmb405 <aTmb405@gmail.com>
2020-10-11 15:50:07 -0500

    Update configureStore function

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,26 @@
 import rootReducer from '../features/rootReducer';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, Store } from 'redux';
 import thunk from 'redux-thunk';
 
 /** 
- * Creates redux store using rootReducer and applyMiddleware. 
- * @const composeEnhancers {function} Uses redux compose in production or redux devtools compose in development
- * @see https://stackoverflow.com/questions/12709074/how-do-you-explicitly-set-a-new-property-on-window-in-typescript.
+ * Creates redux store. 
+ * @param {object} reducer - The root reducer of the app.
+ * @param {function} enhancer - Uses redux compose in production or redux devtools compose in development.
+ * @param {function} middleware - Adds middleware to the store.
+ * @see {@link https://stackoverflow.com/questions/12709074/how-do-you-explicitly-set-a-new-property-on-window-in-typescript|Stack Overflow}
  * */
-export const configureStore = (): any => {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const configureStore = (reducer: any, enhancer: any, middleware: any): Store<any, any> => {
     const store = createStore(
-        rootReducer,
-        composeEnhancers(
-            applyMiddleware(thunk)
+        reducer,
+        enhancer(
+            applyMiddleware(middleware)
         )
     );
     return store;
 }
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = configureStore(rootReducer, composeEnhancers, thunk);
+
+export type AppDispatch = typeof store.dispatch;
+export default store;
