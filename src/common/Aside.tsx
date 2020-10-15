18d5e3a20f643d2502eda2aff3d08e2b494a00b1
aTmb405 <aTmb405@gmail.com>
2020-10-14 22:50:26 -0500

    Convert to .tsx and add type checks

--- src/common/Aside.tsx
+++ src/common/Aside.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Aside = ({children}) => {
+const Aside: React.FunctionComponent = ({ children }) => {
     return (
         <aside className='col-xl-3 col-lg-6 col-md-8 col-sm-10 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-0 mb-2'>
             {children}
