790ac58cf9a31b7672e032b32ed21c064391e949
aTmb405 <aTmb405@gmail.com>
2020-10-15 23:11:22 -0500

    Update type checks for new schema entities

--- src/common/services/schema.ts
+++ src/common/services/schema.ts
@@ -1,14 +1,14 @@
 import { schema } from 'normalizr';
 
-export const user = new schema.Entity('users');
-export const following = new schema.Entity('following');
-export const followers = new schema.Entity('followers', {
+export const user: schema.Entity<string> = new schema.Entity('users');
+export const following: schema.Entity<string> = new schema.Entity('following');
+export const followers: schema.Entity<any> = new schema.Entity('followers', {
     followers: user
 });
-export const reading = new schema.Entity('readings', {
+export const reading: schema.Entity<any> = new schema.Entity('readings', {
     reader: user
 });
-export const subscriptions = new schema.Entity('subscriptions', {
+export const subscriptions: schema.Entity<any> = new schema.Entity('subscriptions', {
     following: user
 });
-export const tags = new schema.Entity('tags');
+export const tags: schema.Entity<string> = new schema.Entity('tags');
