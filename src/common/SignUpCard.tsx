7000fcdb246bfe36c5fc4ab104ead5045e4a875f
aTmb405 <aTmb405@gmail.com>
2020-10-16 11:16:02 -0500

    Convert to .tsx and add type checks

--- src/common/SignUpCard.tsx
+++ src/common/SignUpCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SignUpCard = () => {
+const SignUpCard: React.FunctionComponent = () => {
     return (
         <aside className='col-xl-3 col-lg-6 col-md-8 col-sm-10 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-0 order-xl-1 mb-2'>
             <div className='card text-center border-primary'>
