f09a2249a405569eeb2572de5193d4974e5a0a47
aTmb405 <aTmb405@gmail.com>
2020-10-17 15:01:24 -0500

    Remove error and success actions and replace with alert action

--- src/features/actionTypes.js
+++ src/features/actionTypes.js
@@ -4,9 +4,7 @@ export const RECEIVE_ENTITIES = 'RECEIVE_ENTITIES';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 // errors
-export const ADD_ERROR = 'ADD_ERROR';
-export const REMOVE_ERROR = 'REMOVE_ERROR';
-export const ADD_SUCCESS = 'ADD_SUCCESS';
+export const ADD_ALERT = 'ADD_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 
 // globalReadings
