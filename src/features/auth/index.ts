a1e801ebe3714cb5f5831473c501216c4b795ed8
aTmb405 <aTmb405@gmail.com>
2020-10-17 22:35:02 -0500

    Convert to .ts

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,9 +1,10 @@
 import * as actions from './actions';
 import * as constants from './constants';
 import reducer from './reducer';
+import * as types from './types'
 
 // export { default as AuthForm } from './components/AuthForm';
 // export { default as EmailForm } from './components/EmailForm';
 // export { default as ResetPasswordForm } from './components/ResetPasswordForm';
 // export { default as UpdateForm } from './components/UpdateForm';
-export default { actions, constants, reducer };
+export default { actions, constants, reducer, types };
