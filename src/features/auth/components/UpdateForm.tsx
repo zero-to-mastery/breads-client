807827230057aafed7e252f5e509d38df07561b0
aTmb405 <aTmb405@gmail.com>
2020-10-19 18:23:38 -0500

    Convert to .tsx and add type checks

--- src/features/auth/components/UpdateForm.tsx
+++ src/features/auth/components/UpdateForm.tsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { updateUser } from '../actions';
-import Alert from '../../alerts/Alert';
+import { History } from 'history';
+import { RootState } from '../../rootReducer';
 
-class UpdateForm extends Component {
-    constructor(props) {
+import Alert from '../../alerts/Alert';
+import { updateUser } from '../actions';
+
+type UpdateFormProps = PropsFromRedux & {
+    heading: string | null
+    returnPath: number | null
+    buttonText: string
+    alerts: RootState['alerts']
+    history: History
+}
+
+interface UpdateFormState {
+    username: any
+    image: any
+    [k: string]: any
+}
+
+class UpdateForm extends Component<UpdateFormProps, UpdateFormState> {
+    constructor(props: UpdateFormProps) {
         super(props);
         this.state = {
             username: this.props.currentUser.user.username,
@@ -13,29 +30,22 @@ class UpdateForm extends Component {
         }
     }
 
-    handleChange = e => {
+    handleChange = (e: React.FormEvent<HTMLInputElement>) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.currentTarget.name]: e.currentTarget.value
         });
     };
 
-    handleSubmit = e => {
+    handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         this.props.updateUser(this.props.currentUser.user.id, this.state.image, this.state.username)
-        .then(() => {
-            this.props.history.push(`/${this.props.currentUser.user.id}`);
-        })
-        .catch(() => {
-            return;
-        });
+            .then(() => this.props.history.push(`/${this.props.currentUser.user.id}`))
+            .catch(() => {return});
     }
 
     render() {
         const { username, image } = this.state;
-        const { heading, path, buttonText, alerts, removeAlert } = this.props;
-        // history.listen(() => {
-        //     removeAlert();
-        // });
+        const { heading, returnPath, buttonText, alerts } = this.props;
 
         return (
             <div className='row justify-content-md-center text-center'>
@@ -43,7 +53,7 @@ class UpdateForm extends Component {
                     <form onSubmit={this.handleSubmit}>
                         <h2>Update {heading}</h2>
                         {alerts.message && 
-                            <Alert alerts={alerts} removeAlert={removeAlert}/>
+                            <Alert />
                         }
                         <label htmlFor='image'>Image Url:</label>
                         <input
@@ -69,7 +79,7 @@ class UpdateForm extends Component {
                             {buttonText}
                         </button>
                     </form>
-                    <NavLink exact to={`/${path}`} className='btn btn-warning btn-block mt-2'>
+                    <NavLink exact to={`/${returnPath}`} className='btn btn-warning btn-block mt-2'>
                         Return
                     </NavLink>
                 </div>
@@ -78,10 +88,14 @@ class UpdateForm extends Component {
     }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state: RootState) {
     return {
         currentUser: state.currentUser
     };
 }
 
-export default connect(mapStateToProps, { updateUser })(UpdateForm);
+const connector = connect(mapStateToProps, { updateUser });
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export default connector(UpdateForm);
