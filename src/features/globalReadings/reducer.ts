61328a8dcef11dc04e066d179065e20f8acc4b0c
aTmb405 <aTmb405@gmail.com>
2020-10-21 16:43:05 -0500

    Add function type guards for each action case

--- src/features/globalReadings/reducer.ts
+++ src/features/globalReadings/reducer.ts
@@ -1,22 +1,34 @@
-import { Readings } from '../actions';
+import { Entities, Readings } from '../actions';
 import { RECEIVE_ENTITIES, REMOVE_READING, TOGGLE_FAVORITE } from '../actionTypes';
-import { FavoriteState, ReadingActionTypes, ReadingState } from './types';
+import { FavoriteState, ReadingActionTypes, RemoveReadingState } from './types';
 
 export interface GlobalReadingState {
     [k: string]: Readings
 }
 
+const isReceiveEntitiesAction = (payload: any): payload is {entities: Entities} => {
+    return (payload as {entities: Entities}).entities.readings !== undefined;
+}
+
+const isToggleFavoriteAction = (payload: any): payload is FavoriteState => {
+    return (payload as FavoriteState).user_id !== undefined;
+}
+
+const isRemoveReadingAction  = (payload: any): payload is RemoveReadingState => {
+    return (payload as RemoveReadingState).reading_id !== undefined;
+}
+
 const reading = (state: GlobalReadingState = {}, action: ReadingActionTypes) => {
     switch (action.type) {
         case RECEIVE_ENTITIES:
-            const { entities } = action.payload;
-            if (entities && entities.readings) {
+            if (isReceiveEntitiesAction(action.payload)) {
+                const { entities } = action.payload;
                 return { ...state, ...entities.readings }
             }
             /* falls through */
         case TOGGLE_FAVORITE: 
-            const { id, user_id } = action.payload as FavoriteState;
-            if (id && user_id) {
+            if (isToggleFavoriteAction(action.payload)) {
+                const { id, user_id } = action.payload;
                 const reading = state[id];
                 return {
                     ...state,
@@ -28,8 +40,8 @@ const reading = (state: GlobalReadingState = {}, action: ReadingActionTypes) => 
             }
             /* falls through */
         case REMOVE_READING:
-            const { reading_id } = action.payload as ReadingState;
-            if (reading_id !== undefined) {
+            if (isRemoveReadingAction(action.payload)) {
+                const { reading_id } = action.payload;
                 const { [reading_id]: value, ...other } = state;
                 return other;
             }
