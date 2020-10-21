18e9a06b00da9cc7ef6d5955072ddc702a324554
aTmb405 <aTmb405@gmail.com>
2020-10-20 20:15:55 -0500

    Add action types

---
+++ src/features/globalReadings/types.ts
@@ -0,0 +1,28 @@
+import { ADD_READING, REMOVE_READING, TOGGLE_FAVORITE } from '../actionTypes';
+
+export interface FavoriteState {
+    id: any
+    user_id: any
+}
+
+interface ToggleFavoriteAction {
+    type: typeof TOGGLE_FAVORITE
+    payload: FavoriteState
+}
+
+export interface ReadingState {
+    reading_id?: any
+    user_id: any
+}
+
+interface AddReadingAction {
+    type: typeof ADD_READING
+    payload: ReadingState
+}
+
+interface RemoveReadingAction {
+    type: typeof REMOVE_READING
+    payload: ReadingState
+}
+
+export type ReadingActionTypes = ToggleFavoriteAction | AddReadingAction | RemoveReadingAction;
