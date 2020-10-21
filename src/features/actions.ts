beeaa660426286ac79dfda1231fd5baffb29d02f
aTmb405 <aTmb405@gmail.com>
2020-10-20 22:47:38 -0500

    Update types for ReceiveEntitiesAction

--- src/features/actions.ts
+++ src/features/actions.ts
@@ -1,9 +1,44 @@
 import { RECEIVE_ENTITIES } from './actionTypes';
 
+export interface Users {
+    id: number
+    username: string
+    image: string
+}
+
+export interface Readings {
+    id: number
+    title: string
+    domain: string
+    description: string
+    reading_image: string
+    word_count: number
+    url: string
+    created_at: string
+    favorite: number
+    reader: number
+    tags: string[] | null
+}
+
+export interface Tags {
+    [k: string]: {
+        id: number
+        tag_name: string
+        date: string
+        count: number
+    }
+}
+
+export interface Entities {
+    user?: Users
+    readings?: Readings
+    tags?: Tags
+}
+
 export interface ReceiveEntitiesAction {
     type: typeof RECEIVE_ENTITIES
     payload: {
-        entities: any
+        entities: Entities
     }
     list: any
     id?: any
