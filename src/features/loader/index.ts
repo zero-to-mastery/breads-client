8840cdd13f96cbff1e6781376938fd4b1d741d43
aTmb405 <aTmb405@gmail.com>
2020-10-25 12:24:00 -0500

    Convert to .ts and add type checks

--- src/features/loader/index.ts
+++ src/features/loader/index.ts
@@ -1,5 +1,6 @@
 import * as actions from './actions';
 import * as constants from './constants';
 import reducer from './reducer';
+import * as types from './types';
 
-export default { actions, constants, reducer };
+export default { actions, constants, reducer, types };
