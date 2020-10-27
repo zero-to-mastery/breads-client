71f482b358a7a35d82ed62b93fcd93e3cd297275
aTmb405 <aTmb405@gmail.com>
2020-10-26 20:22:35 -0500

    Convert to .ts and add type checks

--- src/features/notifications/reducer.ts
+++ src/features/notifications/reducer.ts
@@ -1,9 +1,10 @@
 import { LOAD_NOTIFICATIONS, REMOVE_NOTIFICATIONS } from '../actionTypes';
+import { NotificationActionTypes, NotificationState } from './types';
 
-const notification = (state=[], action) => {
+const notification = (state: NotificationState = [], action: NotificationActionTypes) => {
     switch (action.type) {
         case LOAD_NOTIFICATIONS:
-            return [...action.notifications];
+            return [...action.payload.notifications];
         case REMOVE_NOTIFICATIONS:
             return state.filter(notification => notification.subscriber_id !== action.id);
         default:
