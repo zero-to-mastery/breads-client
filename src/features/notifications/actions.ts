571505cf637f17626eccfb3751f08359836354be
aTmb405 <aTmb405@gmail.com>
2020-10-26 20:17:54 -0500

    Convert to .ts and add type checks

---
+++ src/features/notifications/actions.ts
@@ -0,0 +1,44 @@
+import { apiCall } from '../../common/services/api';
+import alerts from '../alerts';
+import { LOAD_NOTIFICATIONS, REMOVE_NOTIFICATIONS } from '../actionTypes';
+import { NotificationState, NotificationActionTypes, PromiseReturnTypes } from './types';
+import { AppDispatch } from '../../app/store';
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from '../rootReducer';
+import { Action } from 'redux';
+
+const { addAlert } = alerts.actions
+
+export const loadNotifications = (notifications: NotificationState): NotificationActionTypes => ({
+    type: LOAD_NOTIFICATIONS,
+    payload: {
+        notifications
+    }
+});
+
+export const removeNotifications = (id: any): NotificationActionTypes => ({
+    type: REMOVE_NOTIFICATIONS,
+    id
+});
+
+export const fetchNotifications = (): ThunkAction<Promise<PromiseReturnTypes>, RootState, unknown, Action<string>> => async (dispatch: AppDispatch, getState: any) => {
+    let {currentUser} = getState();
+    const id = currentUser.user.id;
+    return apiCall('get', `/users/${id}/notifications`)
+        .then(res => {
+            dispatch(loadNotifications(res));
+        })
+        .catch(err => {
+            dispatch(addAlert({message: err.message, type: 'danger'}));
+        });
+}
+
+export const updateNotifications = (sub_id: any): ThunkAction<Promise<PromiseReturnTypes>, RootState, unknown, Action<string>> => async (dispatch: AppDispatch, getState: any) => {
+    let {currentUser} = getState();
+    const id = currentUser.user.id;
+    return apiCall('put', `/users/${id}/notifications`)
+        .then(() => dispatch(removeNotifications(sub_id)))
+        .catch(err => {
+            dispatch(addAlert({message: err.message, type: 'danger'}));
+        });
+}
