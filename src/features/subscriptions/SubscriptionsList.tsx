3c98eee7aafd02da5ac6ca20434c198e226caee7
Ashutosh00710 <starcdwivedi@gmail.com>
2021-03-11 12:47:57 +0530

    translate src/features/subscriptions to typescript

---
+++ src/features/subscriptions/SubscriptionsList.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { fetchSubscriptions, removeSubscription } from "./actions";
+import List from "../../common/List";
+import ListCard from "../../common/ListCard";
+import { getSubscriptions } from "./selectors";
+import { RootState } from "../rootReducer";
+import { match } from "react-router";
+
+type SubscriptionProps = PropsFromRedux & OwnProps;
+
+interface OwnProps {
+    match: match<any>;
+    sub_type: "following" | "followers";
+}
+
+class SubscriptionsList extends Component<SubscriptionProps> {
+    render() {
+        const { friends, removeSubscription, currentUser, match, sub_type } = this.props;
+        let subscriptionsList: JSX.Element[] = [];
+        if (friends) {
+            subscriptionsList = friends[sub_type].map((id: any) => (
+                <ListCard
+                    // refactor like listitem
+                    key={id}
+                    id={id}
+                    removeSubscription={currentUser && removeSubscription.bind(this, currentUser, id)}
+                    subscribed={`${currentUser}` === match.params.id && sub_type !== "followers"}
+                />
+            ));
+        }
+        return <List list_data={subscriptionsList} display="card-columns" />;
+    }
+}
+
+function mapStateToProps(state: RootState, ownProps: OwnProps) {
+    return {
+        friends: getSubscriptions(state, ownProps.match.params.id),
+        currentUser: state.currentUser.user.id,
+    };
+}
+
+const connector = connect(mapStateToProps, {
+    fetchSubscriptions,
+    removeSubscription,
+});
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export default connector(SubscriptionsList);
---
+++ src/features/subscriptions/actions.ts
@@ -0,0 +1,68 @@
+import { apiCall } from "../../common/services/api";
+import alerts from "../alerts";
+import { LOAD_SUBSCRIPTIONS, ADD_SUBSCRIPTION, REMOVE_SUBSCRIPTIONS } from "../actionTypes";
+import { fetchingSubscriptions, removingSubscription, postingNewSubscription, fetchingSubscriptionsIfNeeded } from "./types";
+
+const { addAlert } = alerts.actions;
+
+export const loadSubscriptions = (users: any, id: number) => ({
+    type: LOAD_SUBSCRIPTIONS,
+    users,
+    id,
+});
+
+export const addSubscription = (id: number, user_id: number | null) => ({
+    type: ADD_SUBSCRIPTION,
+    id,
+    user_id,
+});
+
+export const removeSubscriptions = (id: number, user_id: number | null) => ({
+    type: REMOVE_SUBSCRIPTIONS,
+    id,
+    user_id,
+});
+
+export const fetchSubscriptions = (user_id: number): fetchingSubscriptions => async (dispatch) => {
+    return apiCall("get", `/users/${user_id}/subscriptions`)
+        .then((res) => dispatch(loadSubscriptions(res, user_id)))
+        .catch((err) => dispatch(addAlert({ message: err.message, type: "danger" })));
+};
+
+export const removeSubscription = (sub_id: number, pub_id: number): removingSubscription => {
+    return (dispatch) => {
+        return apiCall("delete", `/users/${sub_id}/subscriptions/${pub_id}`)
+            .then(() => {
+                dispatch(removeSubscriptions(pub_id, sub_id));
+                dispatch(addAlert({ message: "Successfully unfollowed", type: "success" }));
+            })
+            .catch((err) => dispatch(addAlert({ message: err.message, type: "danger" })));
+    };
+};
+
+export const postNewSubscription = (sub_id: number): postingNewSubscription => (dispatch, getState) => {
+    let { currentUser } = getState();
+    const user_id = currentUser.user.id;
+    return apiCall("post", `/users/${user_id}/subscriptions`, { sub_id })
+        .then(() => {
+            dispatch(addSubscription(sub_id, user_id));
+            dispatch(addAlert({ message: "Successfully followed", type: "success" }));
+        })
+        .catch((err) => dispatch(addAlert({ message: err.message, type: "danger" })));
+};
+
+const shouldFetchSubscriptions = (state: any, id: number): boolean | undefined => {
+    const subscriptions = state.subscriptions[id];
+    const upToDate = state.subscriptions.upToDate;
+    if (upToDate !== true || !subscriptions) {
+        return true;
+    }
+};
+
+export const fetchSubscriptionsIfNeeded = (id: number): fetchingSubscriptionsIfNeeded => {
+    return (dispatch, getState) => {
+        if (shouldFetchSubscriptions(getState(), id)) {
+            return dispatch(fetchSubscriptions(id));
+        }
+    };
+};
